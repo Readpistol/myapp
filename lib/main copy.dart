d5e3e515c18110ea0776c4ec6b18fc064045a0f0
yaz <admin@example.com>
2021-01-11 16:09:06 +0300

    l

--- lib/main copy.dart
+++ lib/main copy.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Startup Name Generator',
+      title: 'Startup Name Generator2',
       home: RandomWords(),
     );
   }
